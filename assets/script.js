d679121652934beca7a17069210e7c32dae8c2e9
A.Klein <allenkleined@gmail.com>
2022-09-29 21:07:42 -0700

    added functionality comments

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// Assignment code here
+// Arrays variables for the different cases allowed and empty password string 
 const lowerList=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperList=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numericList=["0","1","2","3","4","5","6","7","8","9"];
@@ -7,10 +7,11 @@ const specialList=[" ","!",'"',"#","$","%","&","’","(",")","*","+",",","-","."
 var passString= "";
 
 
-
+//Function to generate password that prompts user
 function generatePassword(){
   var charGenerator=0;
   var passSize= prompt("Enter how long you want the password from 8 to 128 characters");
+  //checks to see if inputted password size is allowed
   while (passSize<8 || passSize>128){
     alert("Incorrect size selection please pick between 8 to 128 characters");
     passSize= prompt("Enter how long you want the password from 8 to 128 characters");
@@ -20,7 +21,7 @@ function generatePassword(){
   var uppercase= confirm("Click ok to include uppercase click cancel to exclude");
   var numeric= confirm("Click ok to include numbers click cancel to exclude");
   var specialChar=confirm("Click ok to include special characters click cancel to exclude");
-  
+  //checks to see if at least one of the character sets allowed was selected
   while(lowercase==false&&uppercase==false&&numeric==false&&specialChar==false){
     alert("You must choose at least one case of characters");
     var lowercase= confirm("Click ok to include lowecase click cancel to exclude");
@@ -28,6 +29,7 @@ function generatePassword(){
     var numeric= confirm("Click ok to include numbers click cancel to exclude");
     var specialChar=confirm("Click ok to include special characters click cancel to exclude");
   }
+  //loop to add a random char from the allowed lists until password length is reached
   while(charGenerator<passSize){
     if(lowercase==true){
       charGenerator++;
