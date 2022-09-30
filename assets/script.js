93ebb415c30e2e40530433dbba6595b51d9b5b8e
A.Klein <allenkleined@gmail.com>
2022-09-29 19:10:04 -0700

    added prompt

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,46 @@
 // Assignment code here
 
 
+
+
+function generatePassword(){
+  var passSize= prompt("Enter how long you want the password from 8 to 128 characters");
+  while (passSize<8 || passSize>128){
+    alert("Incorrect size selection please pick between 8 to 128 characters");
+    passSize= prompt("Enter how long you want the password from 8 to 128 characters");
+  }
+  
+  var lowercase= confirm("Click ok to include lowecase click cancel to exclude");
+  var uppercase= confirm("Click ok to include uppercase click cancel to exclude");
+  var numeric= confirm("Click ok to include numbers click cancel to exclude");
+  var specialChar=confirm("Click ok to include special characters click cancel to exclude");
+  
+  while(lowercase==false&&uppercase==false&&numeric==false&&specialChar==false){
+    alert("You must choose at least one case of characters");
+    var lowercase= confirm("Click ok to include lowecase click cancel to exclude");
+    var uppercase= confirm("Click ok to include uppercase click cancel to exclude");
+    var numeric= confirm("Click ok to include numbers click cancel to exclude");
+    var specialChar=confirm("Click ok to include special characters click cancel to exclude");
+  }
+  
+  if(lowercase==true){
+
+  }
+  else if(uppercase==true){
+
+  }
+  else if(numeric==true){
+
+  }
+  else if(specialChar==true){
+
+  }
+
+  else{
+
+  }
+
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
